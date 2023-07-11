0256703d17834d37f1398bad9bd3c5a7386d78bf
Maher-Batha <maher.battha@gmail.com>
2023-07-11 15:48:45 +0400

    first commit

---
+++ src/SearchForm.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useGlobalContext } from "./Context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="programming"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ src/ThemeToggle.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useGlobalContext } from "./Context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
+};
+
+export default ThemeToggle;
